19e917ff83f524e5323200bbe2fa6dd1d54aa24d
Bambam Kumar Gupta <bambamkumar30082003.ara@gmail.com>
2025-10-23 02:06:44 +0530

    Top K Frequent Elements.cpp

---
+++ Top K Frequent Elements.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    typedef pair<int,int> P;
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+
+        int n = nums.size();
+
+        unordered_map<int,int> mp;
+
+        for(int i=0; i<n; i++){
+            mp[nums[i]]++;
+        }
+
+        priority_queue<P, vector<P>, greater<P>> pq;
+
+        for(auto it: mp){
+            int val = it.first;
+            int freq = it.second;
+
+            pq.push({freq, val});
+            if(pq.size()>k){
+                pq.pop();
+            }
+        }
+
+        vector<int> ans;
+        while(!pq.empty()){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+    }
+};
